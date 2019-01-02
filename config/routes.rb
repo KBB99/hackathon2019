fbffed755f924ecb5d67c951ae36d20ba1c3bee6
KBB99 <kenton@blacutt.com>
2019-01-02 17:21:13 -0500

    Redirect

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :registrations
   root 'registrations#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*path' => redirect('/')
 end
