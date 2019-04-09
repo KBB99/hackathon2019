436bdf9bec924ef32a9d4de0cb93bb18ff0beeb7
Kenton Blacutt <kenton@blacutt.com>
2019-04-09 10:57:51 +0800

    NewLogin

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '*path' => redirect('/')
+  get '*path' => redirect('/')
 
 
 end
